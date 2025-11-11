e11ac6c2592187f118b5990ada03a9bafafd099d
Aniel-J Clariza <ajdcz@proton.me>
2025-11-11 18:08:25 +0400

    Finished simple maths quiz
    Going to frames after break

--- Assessment 1 - Skills Portfolio/Exercises/Ex01_MathsQuiz/main.py
+++ Assessment 1 - Skills Portfolio/Exercises/Ex01_MathsQuiz/main.py
@@ -8,7 +8,30 @@ main.resizable(0,0)
 main.title("Maths Quiz")
 main.configure(background="gray")
 
+mainMenuFrame = Frame(main, background="white")
+
+for frame in mainMenuFrame:
+    frame.place()
+
+mainMenuButton = Button(mainMenuFrame, text="Main Menu", command=lambda x: switch_frame(mainMenuFrame))
+mainMenuButton.place(x=100, y=500, anchor=CENTER)
+
+feedbackLabel = Label(main, text="Welcome")
+feedbackLabel.place(x=400, y=225, anchor=CENTER)
+
+displayProblemLabel = Label(main, text="Welcome")
+displayProblemLabel.place(x=400, y=250, anchor=CENTER)
+
+displayScoreLabel = Label(main, text="Score: ")
+displayScoreLabel.place(x=300, y=300, anchor=CENTER)
+
 correct_answer = None
+attempt_count = 0
+problem_count = 0
+score = 0
+
+def switch_frame(frame):
+    frame.tkraise()
 
 def displayProblem():
     global correct_answer
@@ -25,17 +48,35 @@ def displayProblem():
 
     problem = f"What is {rdInt1} {operator} {rdInt2}?"
 
-    displayProblemLabel = Label(main, text=(problem))
-    displayProblemLabel.place(x=400, y=250, anchor=CENTER)
+    displayProblemLabel.config(text=problem)
 
 def isCorrect():
+    global attempt_count, score, problem_count
     if int(e1.get()) == correct_answer:
-        feedback = Label(main, text="Correct!")
-        feedback.place(x=300, y=300, anchor=CENTER)
-    else:
-        feedback = Label(main, text="Incorrect!")
-        feedback.place(x=300, y=300, anchor=CENTER)
+        feedbackLabel.config(text="Correct!", bg="green")
+        if attempt_count < 1:
+            score += 10
+        else:
+            score += 5
+        displayScoreLabel.config(text=f"Score: {score}")
+        attempt_count = 0
+        problem_count += 1
+        displayProblem()
         
+    else:
+        feedbackLabel.config(text="Try again", bg="red")
+        attempt_count += 1
+    
+    if attempt_count > 1:
+        feedbackLabel.config(text="No Attempts Left!")
+        attempt_count = 0
+        problem_count += 1
+        displayProblem()
+    
+    if problem_count == 10:
+        displayProblemButton.place_forget()
+        submitBtn.place_forget()
+        displayScoreLabel.config(text=f"Total score: {score}")
 
 e1 = Entry(main)
 e1.place(x=400, y=300, anchor=CENTER)
@@ -43,6 +84,9 @@ e1.place(x=400, y=300, anchor=CENTER)
 submitBtn = Button(main, text="Submit", command=isCorrect)
 submitBtn.place(x=500, y=300, anchor=CENTER)
 
-displayProblemButton = Button(main, text="Display Problem", command=displayProblem).place(x=400, y=350, anchor=CENTER)
+displayProblemButton = Button(main, text="Display Problem", command=displayProblem)
+displayProblemButton.place(x=400, y=350, anchor=CENTER)
+
+switch_frame(mainMenuFrame)
 
 mainloop()
